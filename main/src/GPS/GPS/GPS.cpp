8685461bbd649237eb5ed87a4d7697c1844a281b
YutaARAKAWA_dynabook <a.yuuta0413@gmail.com>
2026-06-08 16:20:04 +0900

    GPSのコードを追加

---
+++ main/src/GPS/GPS/GPS.cpp
@@ -0,0 +1,41 @@
+//ヘッダーファイルをインクルード
+#include "GPS.h"
+
+// ライブラリのインクルードは行わない(ヘッダーファイルですでに行っているため)
+
+// 名前空間ここから(ヘッダーファイルと同じ名前空間を定義すること)
+namespace gps {
+    // 変数の定義
+    float latitude; //緯度
+    float longitude; //経度
+    
+    // 自分だけが使う「内部変数」を定義(ヘッダーファイルでは宣言しない)
+
+    void begin() {    //setup()の代わり
+ // Open serial communications and wait for port to open:
+ Serial.begin(57600);
+ while (!Serial) {
+ ; // wait for serial port to connect. Needed for native USB port only
+ }
+
+ Serial.println("Goodnight moon!");
+
+ // set the data rate for the SoftwareSerial port
+ Serial1.begin(9600, SERIAL_8N1, RX1_PIN, TX1_PIN);
+ Serial1.println("Hello, world?");
+    }
+
+    void execute() { // run over and over
+ while (Serial1.available() > 0){
+ char c = Serial1.read();
+ //Serial.print(c);
+ gps.encode(c);
+ if (gps.location.isUpdated()){
+ Serial.print(millis()); Serial.print(", "); Serial.print(gps.location.lat(), 6); Serial.print(", "); Serial.println(gps.location.lng(), 6);
+ }
+ }
+}
+
+    //自作関数を定義する場合はここで行う
+
+    }
